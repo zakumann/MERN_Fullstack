3cbc5c33c579f2c678ea801374cd00e120286739
zakumann <samuel742able@gmail.com>
2024-01-26 18:51:00 +0900

    add validation

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import './NewPlace.css';
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+
 const NewPlace = () => {
     return(
-        <h2>NewPlace Works!</h2>
+        <form className="place-form">
+            <Input 
+                element="input" 
+                type="text" 
+                label="Title" 
+                validators={[VALIDATOR_REQUIRE()]} 
+                errorText="Placse enter a valid title."
+            />
+        </form>
     );
-}
+};
 
 export default NewPlace;
