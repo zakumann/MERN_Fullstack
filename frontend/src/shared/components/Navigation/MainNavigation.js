aaf517bfb0236331f35b13f60a5fe99428408d64
zakumann <samuel742able@gmail.com>
2024-01-28 16:57:45 +0900

    Add update

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -8,40 +8,43 @@ import Backdrop from '../UIElements/Backdrop';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
-    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-    const openDrawerHandler = () => {
-        setDrawerIsOpen(true);
-    };
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
 
-    const closeDrawerHandler = () => {
-        setDrawerIsOpen(false);
-    }
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
 
-    return(
-        <React.Fragment>
-            {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
-            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
-                <nav className="main-navigation__drawer-nav">
-                    <NavLinks />
-                </nav>
-            </SideDrawer>
+  return (
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
 
-            <MainHeader>
-                <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
-                    <span />
-                    <span />
-                    <span />
-                </button>
-                <h1 className="main-navigation__title">
-                    <Link to="/">YourPlaces</Link>
-                </h1>
-                <nav className="main-navigation__header-nav">
-                    <NavLinks />
-                </nav>
-            </MainHeader>
-        </React.Fragment>
-    );
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
 };
 
 export default MainNavigation;
