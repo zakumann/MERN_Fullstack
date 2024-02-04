57e1fc56edf625cde510b356772929ec0674aa08
zakumann <samuel742able@gmail.com>
2024-02-04 11:16:35 +0900

    add authentication

--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -7,7 +7,7 @@ import './NavLinks.css';
 const NavLinks = props => {
   const auth = useContext(AuthContext);
 
-  return <ul className="nav-links">
+  return (<ul className="nav-links">
     <li>
       <NavLink to="/" exact>ALL USERS</NavLink>
     </li>
@@ -26,7 +26,12 @@ const NavLinks = props => {
       <NavLink to="/auth">AUTHENTICATE</NavLink>
     </li>
     )}
+    {auth.isLoggedIn && (
+      <li>
+        <button onClick={auth.logout}>LOGOUT</button>
+      </li>
+    )}
   </ul>
-};
+)};
 
 export default NavLinks;
